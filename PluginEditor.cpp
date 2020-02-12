595fca1ed98c336fc25dd700b03e52ffd3ff14ea
Gabriela Wieliczko <gawieliczko@gmail.com>
2020-02-12 15:54:58 +0100

    Synthesizer with 2 modulators

---
+++ PluginEditor.cpp
@@ -0,0 +1,159 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+GeneratorWt1AudioProcessorEditor::GeneratorWt1AudioProcessorEditor (GeneratorWt1AudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p),
+	keyboardComponent(processor.keyboardState, MidiKeyboardComponent::horizontalKeyboard)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+	addAndMakeVisible(keyboardComponent);
+	setSize(600, 300);
+	startTimer(400);
+
+
+
+	attackSlider.setSliderStyle(Slider::LinearBarVertical);
+	attackSlider.setRange(0.1, 1.0, 0.001);
+	attackSlider.setValue(0.2);
+
+	decaySlider.setSliderStyle(Slider::LinearBarVertical);
+	decaySlider.setRange(0.0, 1.0, 0.001);
+	decaySlider.setValue(0.0);
+
+	sustainSlider.setSliderStyle(Slider::LinearBarVertical);
+	sustainSlider.setRange(0.0, 1.0, 0.1);
+	sustainSlider.setValue(0.0);
+
+	releaseSlider.setSliderStyle(Slider::LinearBarVertical);
+	releaseSlider.setRange(0.01, 10.0, 0.01);
+	releaseSlider.setValue(0.3);
+
+	//Labels
+	addAndMakeVisible(attack);
+	attack.setText("Attack", dontSendNotification);
+	addAndMakeVisible(decay);
+	decay.setText("Decay", dontSendNotification);
+	addAndMakeVisible(sustain);
+	sustain.setText("Sustain", dontSendNotification);
+	addAndMakeVisible(release);
+	release.setText("Release", dontSendNotification);
+
+	//FM labels
+	addAndMakeVisible(mod1Label);
+	mod1Label.setText("Sine FM Modulation", dontSendNotification);
+	addAndMakeVisible(mod2Label);
+	mod2Label.setText("Saw FM modulation", dontSendNotification);
+	
+
+	attack.attachToComponent(&attackSlider, true);
+	decay.attachToComponent(&decaySlider, true);
+	sustain.attachToComponent(&sustainSlider, true);
+	release.attachToComponent(&releaseSlider, true);
+
+	/*mod1Label.attachToComponent(&modulationIndex, false);
+	mod2Label.attachToComponent(&mod2Freq, false);*/
+
+	addAndMakeVisible(&attackSlider);
+	addAndMakeVisible(&decaySlider);
+	addAndMakeVisible(&sustainSlider);
+	addAndMakeVisible(&releaseSlider);
+
+	attackSlider.addListener(this);
+	decaySlider.addListener(this);
+	sustainSlider.addListener(this);
+	releaseSlider.addListener(this);
+
+	//pots for FM synthesis
+	modulationIndex.setSliderStyle(Slider::Rotary);
+	modulationIndex.setRange(0.0, 10.0, 0.1);
+	modulationIndex.setTextBoxStyle(Slider::NoTextBox, false, 90, 0);
+	modulationIndex.setPopupDisplayEnabled(true, false, this);
+	modulationIndex.setTextValueSuffix("Modulation index");
+	modulationIndex.setValue(1.0);
+	addAndMakeVisible(&modulationIndex);
+	modulationIndex.addListener(this);
+
+	freqRatio.setSliderStyle(Slider::Rotary);
+	freqRatio.setRange(1.0, 10.0, 0.5);
+	freqRatio.setTextBoxStyle(Slider::NoTextBox, false, 90, 0);
+	freqRatio.setPopupDisplayEnabled(true, false, this);
+	freqRatio.setTextValueSuffix("Frequency ratio");
+	freqRatio.setValue(1.0);
+	addAndMakeVisible(&freqRatio);
+	freqRatio.addListener(this);
+
+	mod2Amp.setSliderStyle(Slider::Rotary);
+	mod2Amp.setRange(0, 10.0, 0.1);
+	mod2Amp.setTextBoxStyle(Slider::NoTextBox, false, 90, 0);
+	mod2Amp.setPopupDisplayEnabled(true, false, this);
+	mod2Amp.setTextValueSuffix("Saw wave amplitude");
+	mod2Amp.setValue(0.0);
+	addAndMakeVisible(&mod2Amp);
+	mod2Amp.addListener(this);
+
+	mod2Freq.setSliderStyle(Slider::Rotary);
+	mod2Freq.setRange(5.0, 1000.0, 1.0);
+	mod2Freq.setTextBoxStyle(Slider::NoTextBox, false, 90, 0);
+	mod2Freq.setPopupDisplayEnabled(true, false, this);
+	mod2Freq.setTextValueSuffix("Saw wave frequency");
+	mod2Freq.setValue(500.0);
+	addAndMakeVisible(&mod2Freq);
+	mod2Freq.addListener(this);
+}
+
+GeneratorWt1AudioProcessorEditor::~GeneratorWt1AudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void GeneratorWt1AudioProcessorEditor::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    g.setColour (Colours::white);
+    g.setFont (40.0f);
+    //g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
+}
+
+void GeneratorWt1AudioProcessorEditor::resized()
+{
+	int adsrX = 180;
+	keyboardComponent.setBounds(10, 10, getWidth() - 100, 100);
+	attackSlider.setBounds(adsrX, 170, 10,50 );
+	decaySlider.setBounds(adsrX+80, 170, 10, 50);
+	sustainSlider.setBounds(adsrX + 160, 170, 10, 50);
+	releaseSlider.setBounds(adsrX + 240, 170, 10, 50);
+
+	modulationIndex.setBounds(30, 160, 70, 50);
+	freqRatio.setBounds(30, 220, 70, 50);
+	mod2Freq.setBounds(500, 160, 70, 50);
+	mod2Amp.setBounds(500, 220, 70, 50);
+
+	mod1Label.setBounds(10, 110 , 70, 50);
+	mod2Label.setBounds(480, 110, 70, 50);
+    
+}
+
+void  GeneratorWt1AudioProcessorEditor::sliderValueChanged(Slider* slider) {
+	if (slider == &modulationIndex || slider == &freqRatio || slider == &mod2Amp || slider== &mod2Freq) {
+	
+		processor.synthAudioSource.updateVoiceFM(modulationIndex.getValue(), freqRatio.getValue(),mod2Amp.getValue(),mod2Freq.getValue());
+	}
+	else {
+		processor.synthAudioSource.updateVoice(attackSlider.getValue(), decaySlider.getValue(), sustainSlider.getValue(), releaseSlider.getValue());
+	}
+	
+}
---
+++ SynthAudioSource.h
@@ -0,0 +1,292 @@
+/*
+  ==============================================================================
+
+	SynthAudioSource.h
+	Created: 2 Dec 2018 2:47:15pm
+	Author:  korowiow
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+struct SineWaveSound : public SynthesiserSound
+{
+	SineWaveSound() {}
+	bool appliesToNote(int) override { return true; }
+	bool appliesToChannel(int) override { return true; }
+};
+
+//==============================================================================
+struct SineWaveVoice : public SynthesiserVoice
+{
+	// ***
+	SineWaveVoice(const AudioSampleBuffer& wavetableToUse, unsigned int voices) : wavetable(wavetableToUse)
+	{
+		globalLevel = 0.5 / (float)voices;
+		jassert(wavetable.getNumChannels() == 1);
+	}
+
+	//***************wavetable synthesis of 2nd modulator
+	void setFrequency(float frequency, float sampleRate)
+	{
+		auto tableSizeOverSampleRate = wavetable.getNumSamples() / sampleRate;
+		tableDelta = frequency * tableSizeOverSampleRate;
+	}
+
+	forcedinline float getNextSample() noexcept
+	{
+		auto tableSize = wavetable.getNumSamples();
+		auto index0 = (unsigned int)currentIndex;
+		auto index1 = index0 == (tableSize - 1) ? (unsigned int)0 : index0 + 1;
+		auto frac = currentIndex - (float)index0;
+		auto* table = wavetable.getReadPointer(0);
+		auto value0 = table[index0];
+		auto value1 = table[index1];
+		auto currentSample = value0 + frac * (value1 - value0);
+		if ((currentIndex += tableDelta) > tableSize)
+			currentIndex -= tableSize;
+		return currentSample;
+	}
+	//****************************************************
+
+
+
+
+	// ***
+	bool canPlaySound(SynthesiserSound* sound) override
+	{
+		return dynamic_cast<SineWaveSound*> (sound) != nullptr;
+	}
+	void startNote(int midiNoteNumber, float velocity,
+		SynthesiserSound*, int /*currentPitchWheelPosition*/) override
+	{
+		//**
+		currentIndex = 0.0;
+		setFrequency(mod2freq, getSampleRate());
+		//**
+		currentAngle = 0.0;
+		currentModAngle = 0.0;
+
+		level = velocity * globalLevel;
+
+		tailOn = 0.0;
+		tailOff = 0.0;
+		tailOffDecay = 1.0;
+		auto cyclesPerSecond = MidiMessage::getMidiNoteInHertz(midiNoteNumber);
+		auto cyclesPerSample = cyclesPerSecond / getSampleRate();
+		angleDelta = cyclesPerSample * 2.0 * MathConstants<double>::pi;
+
+		auto cyclesPerSecondMod = cyclesPerSecond * (1 / modRatio);
+		auto cyclesPerSampleMod = cyclesPerSecondMod / getSampleRate();
+		angleModDelta = cyclesPerSampleMod * 2.0 * MathConstants<double>::pi;
+
+
+
+	}
+	void stopNote(float /*velocity*/, bool allowTailOff) override
+	{
+		if (allowTailOff) //po komunikacie noteOff ustawiamy tailOff na 1.0, ¿eby uzyskaæ release
+		{
+			if (tailOff == 0.0)
+				tailOff = 1.0;
+		}
+		else
+		{
+			clearCurrentNote();
+			angleDelta = 0.0;
+		}
+
+	}
+	void pitchWheelMoved(int) override {}
+	void controllerMoved(int, int) override {}
+	void renderNextBlock(AudioSampleBuffer& outputBuffer, int startSample, int numSamples) override
+	{
+		if (angleDelta != 0.0) // ***
+		{
+			if (tailOff > 0.0) //obs³uguje moment po zwolnieniu klawisza jeœli chcemy release
+			{
+				while (--numSamples >= 0)
+				{
+					auto currentSample = (float)(std::sin(currentAngle) * level * tailOff);
+					if (sustain != 0.0) {
+						currentSample *= sustain;
+					}
+
+
+					auto currentModSample = (float)(std::sin(currentModAngle) * levelMod);
+					auto mod2 = getNextSample() * mod2level;
+					
+
+					for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
+						outputBuffer.addSample(i, startSample, currentSample);
+					currentAngle += angleDelta * (1 + currentModSample);//* (1 + mod2); //tu mno¿enie przez modulator
+
+					currentModAngle += angleModDelta* (1 + mod2);
+
+					++startSample;
+					tailOff -= float(1 / (release * getSampleRate()));
+					if (tailOff <= 0.005)
+					{
+						clearCurrentNote();
+						angleDelta = 0.0;
+						angleModDelta = 0.0;
+						break;
+					}
+				}
+			}
+			else //obs³uguje dŸwiêk w fazie przytrzymania klawisza
+			{
+				while (--numSamples >= 0)
+				{
+					auto currentModSample = (float)(std::sin(currentModAngle) * levelMod);
+					//if switch on
+					auto mod2 = getNextSample() * mod2level;
+					auto currentSample = (float)(std::sin(currentAngle) * level);
+
+					
+					if (attack != 0.0) {
+						currentSample *= tailOn;
+						if (tailOn <= 1) {
+
+							tailOn += float(1 / (attack * getSampleRate()));
+						}
+						else if (decay != 0.0) {
+							currentSample *= tailOffDecay;
+							if (tailOffDecay > sustain) {
+								tailOffDecay -= float((1 - sustain) / (decay * getSampleRate()));
+							}
+
+
+						}
+						for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
+							outputBuffer.addSample(i, startSample, currentSample);
+						currentAngle += angleDelta * (1 + currentModSample);
+						++startSample;
+
+					}
+					
+					currentModAngle += angleModDelta * (1 + mod2);
+				}
+			}
+		}
+	}
+
+
+
+	float attack = 0.2f, decay = 0.0f, sustain = 0.0f, release = 0.3f;//*****dodajemy parametry modyfikowane przez Gui
+	float levelMod = 1.0f, modRatio = 1.0;
+	float mod2level = 0.0f, mod2freq = 500.0f;
+private:
+	float tailOn = 0.0f;
+	float tailOffDecay = 0.0f;
+	float tailOff = 0.0; // ***
+
+	//int currentFrequency=0;
+
+	
+	double currentAngle = 0.0, angleDelta = 0.0;
+	double currentModAngle = 0.0, angleModDelta = 0.0;
+	float level = 0.0f, globalLevel = 0.0f;
+
+	//wavetable synthesis of second modulator (saw)
+	const AudioSampleBuffer& wavetable;
+	float currentIndex = 0.0f, tableDelta = 0.0f;
+};
+
+
+
+//==============================================================================
+class SynthAudioSource : public AudioSource
+{
+public:
+	SynthAudioSource(MidiKeyboardState& keyState)
+		: keyboardState(keyState)
+	{
+		for (auto i = 0; i < numVoices; ++i)
+			voices.push_back(new SineWaveVoice(signalTable, numVoices));
+		for (auto i = 0; i < numVoices; ++i)
+			synth.addVoice(voices[i]);
+		synth.addSound(new SineWaveSound());
+	}
+
+	void updateVoice(float a, float d, float s, float r) {
+		for (auto i = 0; i < numVoices; ++i) {
+			voices[i]->attack = a;
+			voices[i]->decay = d;
+			voices[i]->sustain = s;
+			voices[i]->release = r;
+		}
+	}
+	void updateVoiceFM(float modIdx, float modRat,float mod2amp,float mod2freq) {
+		for (auto i = 0; i < numVoices; ++i) {
+			voices[i]->levelMod = modIdx;
+			voices[i]->modRatio = modRat;
+			voices[i]->mod2level = mod2amp;
+			voices[i]->mod2freq = mod2freq;
+
+		}
+	}
+	void setUsingSineWaveSound()
+	{
+		synth.clearSounds();
+	}
+	void prepareToPlay(int /*samplesPerBlockExpected*/, double sampleRate) override
+	{
+		synth.setCurrentPlaybackSampleRate(sampleRate);
+		createWavetable(); // ***
+	}
+	void releaseResources() override {}
+	void getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill) override
+	{
+		bufferToFill.clearActiveBufferRegion();
+		MidiBuffer incomingMidi;
+		midiCollector.removeNextBlockOfMessages(incomingMidi, bufferToFill.numSamples);
+		keyboardState.processNextMidiBuffer(incomingMidi, bufferToFill.startSample,
+			bufferToFill.numSamples, true);
+		synth.renderNextBlock(*bufferToFill.buffer, incomingMidi,
+			bufferToFill.startSample, bufferToFill.numSamples);
+	}
+	MidiMessageCollector* getMidiCollector()
+	{
+		return &midiCollector;
+	}
+	MidiMessageCollector midiCollector;
+
+	std::vector< SineWaveVoice*>voices;
+
+
+
+private:
+	MidiKeyboardState& keyboardState;
+	Synthesiser synth;
+
+	// ***
+	int tableSize = 128;
+	int numVoices = 4;
+	AudioSampleBuffer signalTable;
+	void createWavetable()
+	{
+		signalTable.setSize(1, tableSize);
+		auto* samples = signalTable.getWritePointer(0);
+		//auto angleDelta = MathConstants<double>::twoPi / (double)(tableSize - 1);
+		//auto currentAngle = 0.0;
+		auto currentValue = 1.0;
+		for (auto i = 0; i < tableSize; ++i)
+		{
+			/*auto sample = std::sin(currentAngle);
+			samples[i] = (float)sample;
+			currentAngle += angleDelta;*/
+			auto sample = currentValue;
+			samples[i] = (float)sample;
+			currentValue -= float(1.0/tableSize);
+
+		}
+	}
+
+
+	// ***
+};
